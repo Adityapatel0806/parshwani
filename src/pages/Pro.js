f4845db786c4c5ee47b1df963dd73ddc1dc02f57
Adi <pateladitya949@gmail.com>
2023-06-19 22:46:36 +0530

    added pro page

---
+++ src/pages/Pro.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { datas } from "FinalJson";
+
+export default function Pro() {
+  const { type } = useParams();
+
+  var result = {};
+  // const result = data.find((product) => product.name === name);
+  const results = datas.find((data) =>
+    data.products.some((product) => {
+      if (product.category === type) {
+        console.log(product);
+        result = product;
+      }
+    })
+  );
+//   console.log(result);
+  return <div>
+  {result.category}
+  {result.data.map((item) =><Link to={`${item.name}`}>{item.name}</Link>)}
+  </div>;
+}
